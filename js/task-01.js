bbe477833e6a7d9f360ca9649a1237ce4789cd40
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-08 12:49:29 +0200

    1+ доробити

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+
+const categories = document.querySelectorAll('.item');
+console.log(categories);
+console.log('Number of categories:', categories.length);
+
+const categoriesName = document.querySelectorAll('.item h2');
+ const categoriesUl = document.querySelectorAll('.item ul');
+
+categoriesName.forEach(element => {
+
+    console.log(`Category: ${element.textContent}
+    `);
+    
+})
+categoriesUl.forEach(element => {
+    console.log(`Elements: ${element.children.length}`);
+})
+
+
+
+
+
