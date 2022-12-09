1138a376d74b9747744ea4e82794ac2ea826e6db
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-09 21:28:49 +0200

    8+

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onSearch);
+function onSearch(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені");
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);  
+    event.currentTarget.reset();
+}
