65377ae23b229a932b0e458c4e1c4ac781ef6056
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-09 18:56:07 +0200

    6+

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputValid = document.querySelector('#validation-input');
+const atribute = document.querySelector('input[data-length="6"]');
+const atributeValue = inputValid.dataset.length;
+
+
+
+inputValid.addEventListener('blur', () => {
+    if (inputValid.value.length.toString() === atributeValue) {
+        inputValid.classList.add("valid");
+        inputValid.classList.remove("invalid");
+    } else
+        inputValid.classList.add("invalid");
+      
+});
+
