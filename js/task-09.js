42e391620b7a62f51075cd80c725d39b0e982676
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-11 19:58:31 +0200

    10++

--- js/task-09.js
+++ js/task-09.js
@@ -2,14 +2,20 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const bodyColor = document.querySelector("body")
-const colorFon = document.querySelector('.widget');
 const button = document.querySelector('.change-color');
 const span = document.querySelector('.color');
 
-colorFon.addEventListener('click', onClick)
+button.addEventListener('click', onClick)
 
 function onClick(event) {
-  bodyColor.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
+  let color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  span.textContent = color;
+  button.style.backgroundColor = color;
 }
+
+
+
+
+
 
