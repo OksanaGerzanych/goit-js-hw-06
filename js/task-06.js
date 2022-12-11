ead3a0669ebb9b599d9292ee215f62723b03763d
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-11 20:56:53 +0200

    6 - виправлено

--- js/task-06.js
+++ js/task-06.js
@@ -3,23 +3,14 @@ const atribute = document.querySelector('input[data-length="6"]');
 const atributeValue = inputValid.dataset.length;
 
 
-
 inputValid.addEventListener('blur', () => {
     if (inputValid.value.length.toString() === atributeValue) {
         inputValid.classList.add("valid");
-        
-        if (inputValid.classList.contains('invalid')) {
-            inputValid.classList.remove('invalid')
-        }
-        inputValid.classList.remove('invalid');
+        inputValid.classList.remove('invalid')
        
     } else {
-        if (inputValid.classList.contains('valid')) {
-            inputValid.classList.remove('valid')
-        }
         inputValid.classList.add('invalid');
+        inputValid.classList.remove('valid')
     }
-       
-      
+        
 });
-
